18e78f3ad5826f7721b41b0bad58aef206857355
Andrew <lucas.andrew.j@proton.me>
2025-04-29 23:32:29 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIconRegistry, MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    MatSidenavModule, MatToolbar, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'troop-website';
+  opened = 'opened';
 }
