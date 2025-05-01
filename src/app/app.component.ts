94ca170554ae1918505b8eea2c7433411802ba02
Andrew <lucas.andrew.j@proton.me>
2025-04-30 21:59:34 -0700

    Updates sidenav to use list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIconRegistry, MatIconModule} from '@angular/material/icon';
+import {MatNavList} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
-    MatSidenavModule, MatToolbar, MatIconModule],
+    MatSidenavModule, MatToolbar, MatIconModule, MatNavList],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
