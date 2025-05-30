38246e3dbca318be67f1dcde2fd578e0bf840b04
Andrew <lucas.andrew.j@proton.me>
2025-05-30 14:32:12 -0700

    Adds framework for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbar} from '@angular/material/toolbar';
-import {MatIconRegistry, MatIconModule} from '@angular/material/icon';
-import {MatNavList} from '@angular/material/list';
+import { RouterLink, RouterOutlet} from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-    MatSidenavModule, MatToolbar, MatIconModule, MatNavList],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbar,
+    MatIconModule,
+    MatNavList,
+    MatListItem,
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {UpcomingEventsComponent} from './upcoming-events/upcoming-events.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'upcoming-events',
+    component: UpcomingEventsComponent,
+  },
+];
