fe556f2e5e9563f8042c080bf6c5f0d86dbc693a
Andrew <lucas.andrew.j@proton.me>
2025-06-25 21:49:58 -0700

    Adds buttons and correct layout for add event dialog

--- src/app/events/add-event-dialog/add-event-dialog.component.ts
+++ src/app/events/add-event-dialog/add-event-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDialogContent} from '@angular/material/dialog';
+import {MatDialogActions, MatDialogContent} from '@angular/material/dialog';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatOption} from '@angular/material/select';
 import {provideNativeDateAdapter} from '@angular/material/core';
@@ -8,6 +8,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {KeyValuePipe} from '@angular/common';
 import {EventType} from '../event.model';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-add-event-dialog',
@@ -19,6 +20,8 @@ import {EventType} from '../event.model';
     MatSelectModule,
     MatInputModule,
     KeyValuePipe,
+    MatDialogActions,
+    MatButton,
   ],
   providers: [
     MatDatepickerModule,
@@ -30,4 +33,12 @@ import {EventType} from '../event.model';
 export class AddEventDialogComponent {
 
   protected readonly EventType = EventType;
+
+  addEvent() {
+
+  }
+
+  cancel() {
+
+  }
 }
